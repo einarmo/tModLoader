6403fec89ccf435a68f6f7cc988bde15f6e31795
Mirsario <me@mirsar.io>
2022-01-11 05:13:51 +0300

    EM repair.

--- ExampleMod/Content/Tiles/AutoClentaminator.cs
+++ ExampleMod/Content/Tiles/AutoClentaminator.cs
@@ -81,7 +81,7 @@ namespace ExampleMod.Content.Tiles
 		}
 
 		public override void KillMultiTile(int i, int j, int frameX, int frameY) {
-			Item.NewItem(i * 16, j * 16, 48, 48, ModContent.ItemType<Items.Placeable.AutoClentaminator>());
+			Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 48, 48, ModContent.ItemType<Items.Placeable.AutoClentaminator>());
 			ModContent.GetInstance<AutoClentaminatorTE>().Kill(i, j);
 		}
 	}
